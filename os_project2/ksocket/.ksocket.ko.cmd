9be3a4e3cc3ed97f6fcc0c0d3475e032c9544473
archielu <ntub07902045@gmail.com>
2020-06-19 18:43:20 +0800

    ksocket

---
+++ os_project2/ksocket/.ksocket.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/archie0120/os_project2/ksocket/ksocket.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/archie0120/os_project2/ksocket/ksocket.ko /home/archie0120/os_project2/ksocket/ksocket.o /home/archie0120/os_project2/ksocket/ksocket.mod.o ;  true
---
+++ os_project2/ksocket/.tmp_versions/ksocket.mod
@@ -0,0 +1,3 @@
+/home/archie0120/os_project2/ksocket/ksocket.ko
+/home/archie0120/os_project2/ksocket/ksocket.o
+
